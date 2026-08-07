a575992be66b6d1cf639a909d7d7b6111e6ee3b0
axionneuralis-a11y <axionneuralis@gmail.com>
2026-08-07 18:23:58 +0700

    delete all files

--- models/calculator_model.py
+++
@@ -1,9 +0,0 @@
-"""Model riwayat kalkulator (tabel calc_history)."""
-
-from models.base_model import BaseModel
-
-
-class CalculatorModel(BaseModel):
-    """Akses riwayat perhitungan."""
-
-    table_name: str = "calc_history"

7ef0d3a88cf20505acb9c955f7bc79cde3835a6c
axionneuralis-a11y <axionneuralis@gmail.com>
2026-08-07 15:33:41 +0700

    Implement BasicCodeEditor with syntax highlighting
    Added a BasicCodeEditor class that extends CodeInput with Python syntax highlighting and custom styling.

--- components/code_editor.py
+++ components/code_editor.py
@@ -1,1 +1,10 @@
-halo
+# components/code_editor.py
+from kivy.uix.codeinput import CodeInput
+from pygments.lexers import PythonLexer
+
+class BasicCodeEditor(CodeInput):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.lexer = PythonLexer()  # Default sementara
+        self.font_name = 'RobotoMono-Regular'
+        self.background_color = (0.07, 0.07, 0.07, 1) # Dark mode base
